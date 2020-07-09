bdccc91acc523bd4bae40316603356dcf1772a74
jmjpro <jmjpro@gmail.com>
2020-07-09 18:21:51 +0300

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "airbnb",
     "plugin:import/errors",
     "plugin:import/warnings",
--- TODO.md
+++ TODO.md
@@ -2,3 +2,4 @@ set up prettier
 set up debugging
 add support for mixed JS/TS project
 add watcher for starting service
+
